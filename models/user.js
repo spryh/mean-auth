e985fccac613bfca7f64d0a0847adca81ac30e0b
Paul Scarpa <spryh@Bootstrap.local>
2017-12-24 13:34:49 -0800

    thru 2.10 - bCrypting passwords

--- models/user.js
+++ models/user.js
@@ -1,4 +1,7 @@
+//import { userInfo } from 'os';
+
 var mongoose = require('mongoose')
+var bcrypt = require('bcrypt')
 
 //Trim removes whitespace
 var UserSchema = new  mongoose.Schema({
@@ -6,6 +9,19 @@ var UserSchema = new  mongoose.Schema({
     name: {type: String, required: true, trim: true},
     favoriteBook: {type: String, required: true, trim: true},
     password: {type: String, required: true}
+})
+
+// Hash password before storage with Save pre hook
+UserSchema.pre('save', function(next){
+    var user = this
+    // 10 is a good balance between security and performance
+    bcrypt.hash(user.password, 10, (err,hash)=>{
+        if(err) {return next (err)}
+        else {
+            user.password = hash
+            next()
+        }
+    })
 })
 
 var User = mongoose.model('User', UserSchema)
